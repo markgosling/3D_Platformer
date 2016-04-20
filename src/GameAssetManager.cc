b5b2fe7ecdf4950048a5904706a90d4acd4de803
Mark Gosling <m_gosling16@hotmail.com>
2016-04-20 14:31:20 +0100

    re-added boost/python.hpp to GameAssetManager.cc

--- src/GameAssetManager.cc
+++ src/GameAssetManager.cc
@@ -3,7 +3,7 @@
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-//#include <boost/python.hpp>
+#include <boost/python.hpp>
 
 /**
  * @file GameAssetManager.cc
